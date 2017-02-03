473db8da04c544e70f3de307a0c3d3e6220a8f94
Patrick Allen <allenpatrick86@gmail.com>
2017-02-03 15:32:21 -0800

    add working notes for ideas

---
+++ Notes/Auth-Credentials.rs
@@ -0,0 +1,20 @@
+// MY ATTEMPT:
+// #[allow(dead_code)]
+// fn auth() -> Authorization {
+//     Authorization(
+//        Basic {
+//            username: "influential_apitest".to_owned(),
+//            password: Some("mpzuga2ncpik".to_owned())
+//        }
+//     );
+// }
+// EXAMPLE FROM SITE:
+// let mut headers = Headers::new();
+// headers.set(
+//    Authorization(
+//        Basic {
+//            username: "Aladdin".to_owned(),
+//            password: Some("open sesame".to_owned())
+//        }
+//    )
+// );
