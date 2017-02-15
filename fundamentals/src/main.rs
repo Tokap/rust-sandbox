7ecaaaa1ef6ffd8f100ee756ff0f6f558b17023f
Patrick Allen <allenpatrick86@gmail.com>
2017-02-14 21:27:43 -0800

    minor rename

--- fundamentals/src/main.rs
+++ fundamentals/src/main.rs
@@ -10,6 +10,8 @@ extern crate serde_json;
 extern crate serde_derive;
 
 extern crate serde;
+extern crate jsonsql;
+use jsonsql as patsql;
 
 // extern crate rustc_serialize;
 // use rustc_serialize::json;
@@ -18,62 +20,39 @@ use serde_json::*;
 use serde::{Serialize, Deserialize};
 use json::*;
 
+use mysql::{Pool};
+
 #[derive(Debug, Default, Serialize, Deserialize)]
 pub struct SomePerson {
     name: String,
     age: u32,
 }
 
+
+
 fn main() {
-    // let john = json!({
-    //   "name": "John Doe",
-    //   "age": 43,
-    // });
+    let test_json: String = "{\"network\":\"twitter\",\"network_id\":\"1713789122\",\"network_username\":\"buck_johnson\",\"follower_count\":\"851196\",\"correlation_id\":\"97a2e620-6d45-46bb-bf16-5c2c6082897d\",\"archive_id\":\"2\",\"type_id\":\"2\"}".to_string();
 
-    /*** Some notes about using the Json library ***/
-    let stringify = json::stringify("foobar cat".to_string()); // encapsulates in quotes
-    assert_eq!(json::stringify(json::Null), "null"); // use of null
-    let data = vec![1,2,3];
-    assert_eq!(json::stringify(data), "[1,2,3]");
+    let pool: Pool = build_pool("127.0.0.1", "ip_brolytics", "root", 3306);
 
-    // Adding Data to Objects
-    let mut data = json::JsonValue::new_object();
-    data["answer"] = 42.into();
-    data["foo"] = "bar".into();
-    assert_eq!(data.dump(), r#"{"answer":42,"foo":"bar"}"#);
+    let table: String = String::from("account_data_archive");
 
+    let mut tuple_vec: Vec<(String, String)> = Vec::new();
+        tuple_vec.push(("network".to_string(), "twitter".to_string()));
+        tuple_vec.push(("network_id".to_string(), "8964323".to_string()));
+        tuple_vec.push(("network_username".to_string(), "jimbo".to_string()));
+        tuple_vec.push(("follower_count".to_string(), "456733".to_string()));
+        tuple_vec.push(("correlation_id".to_string(), "89J6X43C23".to_string()));
+        tuple_vec.push(("archive_id".to_string(), "1".to_string()));
+        tuple_vec.push(("type_id".to_string(), "2".to_string()));
 
-    // My Stuff:
-    let json_string = json::parse(r#"
-    {
-        "name": "john",
-        "age": 25,
-        "data": {
-            "debt": 10000.00,
-            "income": 400.00
-        }
-    }
-    "#).unwrap();
+    // let print_statement: String = patsql::simple_json_insert(table, test_json);
+    // println!("My Print String Looks Like: {:?}",print_statement);
 
-    let instantiated = object!{
-    "code" => 200,
-    "success" => true,
-    "payload" => object!{
-        "features" => array![
-            "awesome",
-            "easyAPI",
-            "lowLearningCurve"
-            ]
-        }
-    };
+    // let my_return = json_write_to_table(table, test_json, pool);
+    // println!("My RETURN: {:?}", my_return);
 
-
-    // let j = serde_json::to_string(&john);
-    // let t = assert_eq!(json_string["data"]["debt"], 10000);
-
-    println!("JSON Stringy: {:?}", stringify);
-
-    println!("json String!!: {:?}", json_string["data"]["debt"] );
-    println!("Object Creation!!: {:?}", instantiated );
+    let return_value: String = get_by_param_with_handling("id", "1", "account_data_archive", pool);
+    println!("My Outcome Looks Like: {}",return_value);
 
 }
